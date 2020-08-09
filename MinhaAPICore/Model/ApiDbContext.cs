d8c0ffa8d46f2cb7aa5a2dab3515f28e91b868be
Flavio Lima do nascimento <flavioflnlima@gmail.com>
2020-08-09 20:11:51 -0300

    feat: finalizado CRUD inicial

---
+++ MinhaAPICore/Model/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinhaAPICore.Model
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
---
+++ MinhaAPICore/Model/Supplier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MinhaAPICore.Model
+{
+    public class Supplier
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage =  "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14,ErrorMessage = "Ocampo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Document { get; set; }
+        public int SupplierType { get; set; }
+        public bool Active { get; set; }
+
+
+    }
+}
