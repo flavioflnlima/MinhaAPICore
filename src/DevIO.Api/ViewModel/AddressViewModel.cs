6f4b30be998f7b1cb701aaec6692eaebd4fc07c1
flavioflnlima <flavioflnlima@gmail.com>
2020-09-30 23:45:07 -0300

    retornando estudos

---
+++ src/DevIO.Api/ViewModel/AddressViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.Api.ViewModel
+{
+    public class AddressViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Numero { get; set; }
+
+        public string Complement { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string District { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(8, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 8)]
+        public string Cep { get; set; }        
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string State { get; set; }
+
+        public Guid SupplierId { get; set; }
+    }
+}
---
+++ src/DevIO.Api/ViewModel/SupplierViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.Api.ViewModel
+{
+    public class SupplierViewModel
+    {
+        [Key]
+        public Guid ID{ get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Name { get; set; }    
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14,ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Document { get; set; }
+
+        public int TypeSupplier { get; set; }
+        public AddressViewModel Address { get; set; }
+        public bool Active { get; set; }
+        public IEnumerable<ProductsViewModel> Products{ get; set; }
+    }
+}
